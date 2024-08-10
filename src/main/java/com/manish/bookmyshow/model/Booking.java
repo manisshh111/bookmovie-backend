12c904f40f96d7cdfb4b79a2360de4e740c8ef93
manish <manish.sharma22221@gmail.com>
2024-08-10 23:19:48 +0530

    Booking model updated

--- src/main/java/com/manish/bookmyshow/model/Booking.java
+++ src/main/java/com/manish/bookmyshow/model/Booking.java
@@ -1,6 +1,7 @@
 package com.manish.bookmyshow.model;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -13,7 +14,11 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 
 @Entity
 @Table(name = "bookings")
@@ -35,5 +40,12 @@ public class Booking {
 	@OneToOne
 	@JoinColumn(name = "payment_id")
 	Payment payment;
+	
+	@Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+	@NotEmpty(message = "Email cannot be empty")
+	private String email;
+	
+	 @Size(min=0,max=10)
+	 String mobileNo;
 
 }
